140cf5756c90781deac321268a87f57ac3fd3ecb
Rafael Kauch <kauchrafael0001@gmail.com>
2023-02-04 16:45:41 -0300

    added up safe context.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,26 @@
 import { Container, Logo, BackIcon, BackButton } from './styles';
 
 import logo from "@assets/logo.png";
+import { useNavigation } from '@react-navigation/native';
 
 type Props = {
     showBackButton?: boolean;
 }
 
 export function Header({ showBackButton = false}: Props){
+
+    const navigation = useNavigation();
+
+    function handleGoBack(){
+        // navigation.goBack();
+        navigation.navigate('groups');
+    }
+
     return (
         <Container>
             {
                 showBackButton &&
-                <BackButton>
+                <BackButton onPress={handleGoBack}>
                     <BackIcon />
                 </BackButton>
             }
--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -4,8 +4,17 @@ import { Header } from "@components/Header"
 import { Highlight } from '@components/Highlight';
 import { Button } from '@components/Button';
 import { Input } from '@components/Input';
+import { useNavigation } from '@react-navigation/native';
+import { useState } from 'react';
 
 export function NewGroup(){
+    const [group, setGroup] = useState('');
+
+    const navigation = useNavigation();
+
+    function handleNew(){
+        navigation.navigate('players', { group });
+    }
     return (
         <Container>
             <Header showBackButton/>
@@ -20,11 +29,13 @@ export function NewGroup(){
 
                 <Input 
                     placeholder="Nome da turma"
+                    onChangeText={setGroup}
                 />
 
                 <Button
                     title="Criar"
                     style={{marginTop: 16}}
+                    onPress={handleNew}
                 />
             </Content>
         </Container>
